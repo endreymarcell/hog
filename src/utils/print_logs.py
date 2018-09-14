7ca2df4b6e07815f2317b1500fd92c0d6e61dbf9
Marcell Endrey <marcell.endrey@prezi.com>
2018-09-14 18:12:11 +0200

    add file printing logic

--- src/utils/print_logs.py
+++ src/utils/print_logs.py
@@ -1,5 +1,13 @@
+import gzip
 from typing import List
 
 
 def print_logs(filenames: List[str]) -> None:
-    pass
+    for filename in filenames:
+        if filename.endswith(".gz"):
+            with gzip.open(filename, "rt") as file:
+                for line in file.read().split("\n"):
+                    print(line)
+        else:
+            with open(filename) as file:
+                print(file.read())
