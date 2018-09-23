4cc20c2ec336e76506dcc39449db4c6b5e7ddf80
Marcell Endrey <marcell.endrey@prezi.com>
2018-09-23 16:34:27 +0200

    implement absolute specifiers, add tests

--- src/utils/get_filenames.py
+++ src/utils/get_filenames.py
@@ -3,6 +3,12 @@ from typing import List, Tuple, Optional
 
 from const import SCRIBE_ROOT
 
+TIMESTAMP_LENGTH_IN_FILENAME = 16
+
+
+class NoFileMatchesAbsoluteSpecifierException(Exception):
+    pass
+
 
 def list_logfiles(directory: str) -> List[str]:
     return list(
@@ -19,32 +25,38 @@ def get_filenames(logcategory: str, interval: str) -> List[str]:
 
 def select_filenames_for_interval(filenames: List[str], interval: str) -> List[str]:
     if ":" in interval:
-        start_index, end_index = _get_indexes_for_interval(*interval.split(":"))
+        start_index, end_index = _get_indexes_for_interval(
+            filenames, *interval.split(":")
+        )
         return filenames[start_index:end_index]
     else:
-        index = _get_index_for_single(interval)
+        index = _get_index_for_single(filenames, interval)
         return [filenames[index]]
 
 
 def _get_indexes_for_interval(
-    start_specifier: str, end_specifier: str
+    filenames: List[str], start_specifier: str, end_specifier: str
 ) -> Tuple[Optional[int], Optional[int]]:
     start_index = (
-        _get_index_for_single(start_specifier) if start_specifier != "" else None
+        _get_index_for_single(filenames, start_specifier)
+        if start_specifier != ""
+        else None
     )
     end_index = (
-        _get_index_for_single(end_specifier) + 1 if end_specifier != "" else None
+        _get_index_for_single(filenames, end_specifier) + 1
+        if end_specifier != ""
+        else None
     )
     if end_index == 0:
         end_index = None
     return start_index, end_index
 
 
-def _get_index_for_single(specifier: str) -> int:
+def _get_index_for_single(filenames: List[str], specifier: str) -> int:
     return (
         _get_index_for_relative_single(specifier)
         if specifier.startswith("-")
-        else _get_index_for_absolute_single(specifier)
+        else _get_index_for_absolute_single(filenames, specifier)
     )
 
 
@@ -52,5 +64,20 @@ def _get_index_for_relative_single(specifier: str) -> int:
     return int(specifier)
 
 
-def _get_index_for_absolute_single(specifier: str) -> int:
-    pass
+def _get_index_for_absolute_single(filenames: List[str], specifier: str) -> int:
+    filenames_and_slugs = [
+        (filename, _get_slug_for_filename(filename)) for filename in filenames
+    ]
+    for index, (filaneme, slug) in enumerate(reversed(filenames_and_slugs)):
+        if slug.endswith(specifier):
+            return -index - 1
+    else:
+        raise NoFileMatchesAbsoluteSpecifierException
+
+
+def _get_slug_for_filename(filename: str) -> str:
+    return (
+        filename.replace("_", "-")
+        .replace(".gz", "")
+        .replace("000", "")[-TIMESTAMP_LENGTH_IN_FILENAME:]
+    )
