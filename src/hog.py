924186c68cc18c6f2e87fddc8464bd38e888ecc9
Marcell Endrey <marcell.endrey@prezi.com>
2018-09-28 11:58:24 +0200

    parse args properly, add verbose option

--- src/hog.py
+++ src/hog.py
@@ -5,17 +5,28 @@ from utils.args import parse_args
 from utils.get_filenames import get_filenames
 from utils.get_logcategory import list_logcategories, get_logcategory
 from utils.print_logs import print_logs
+from sys import stderr
 
 
 def main() -> None:
     args = parse_args()
+    if args.is_verbose:
+        print(f"---hog logcategory specifier: {args.logcategory}", file=stderr)
+        print(f"---hog interval specifier: {args.interval}", file=stderr)
     logcategories = list_logcategories()
     logcategory: str = get_logcategory(
         query=args.logcategory, logcategories=logcategories
     )
+    if args.is_verbose:
+        print(f"---hog logcategory: {logcategory}", file=stderr)
     filenames: List[str] = get_filenames(
         logcategory=logcategory, interval=args.interval
     )
+    if args.is_verbose:
+        print("---hog filenames:", file=stderr)
+        for filename in filenames:
+            print(filename, file=stderr)
+        print("---hog file contents:", file=stderr)
     print_logs(logcategory, filenames)
 
 
--- src/utils/args.py
+++ src/utils/args.py
@@ -5,8 +5,22 @@ from const import QUERY_RE_PATTERN, INTERVAL_RE_PATTERN
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="Easily specify some log files and print their contents to stdout."
+    parser = _setup_parser()
+    args = _parse_args_intelligently(parser)
+    if _are_args_valid(args.logcategory, args.interval):
+        return args
+    else:
+        raise argparse.ArgumentError(argument=None, message="Invalid args")
+
+
+def _setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="hog", add_help=False)
+    parser.add_argument(
+        "-v",
+        help="verbose logging for debug purposes",
+        dest="is_verbose",
+        action="store_true",
+        default=False,
     )
     parser.add_argument(
         "logcategory", help="glob-like string to specify the log category"
@@ -17,14 +31,25 @@ def parse_args() -> argparse.Namespace:
         default="-1",
         help="specify which log files should be read",
     )
-    args = parser.parse_args()
-    if are_args_valid(args.logcategory, args.interval):
-        return args
-    else:
-        raise argparse.ArgumentError("Invalid args")
+    return parser
 
 
-def are_args_valid(logcategory: str, interval: str) -> bool:
+def _parse_args_intelligently(parser: argparse.ArgumentParser) -> argparse.Namespace:
+    """Parse arguments, including complex interval specifiers
+
+    argparse can't parse parameters that begin with a dash, eg. interval
+    queries such as -1: as it tries to identify them as optional argument
+    names because of the leading dash. Workaround is to parse only the
+    known args and rescue the lost interval specifier parameter from the
+    unkonwn args list.
+    """
+    args, unknown_args = parser.parse_known_args()
+    if len(unknown_args) == 1:
+        args.interval = unknown_args[0]
+    return args
+
+
+def _are_args_valid(logcategory: str, interval: str) -> bool:
     return (
         re.match(QUERY_RE_PATTERN, logcategory) is not None
         and re.match(INTERVAL_RE_PATTERN, interval) is not None
